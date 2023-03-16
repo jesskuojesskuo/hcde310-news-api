d7b590e9d46185618cf2dd5f0c602d5fa7f08350
Austin Lee <austlee@uw.edu>
2023-03-16 11:57:53 +0800

    Search

--- app.py
+++ app.py
@@ -12,10 +12,34 @@ app = Flask(__name__)
 def index():
     search = "test"
     return render_template('search.html', results = search)
+    # return render_template('search.html', results = search)
 
-@app.route("/search")
+# @app.route("/search")
+# def search():
+#     input = request.args.get("search")
+#     input_category = request.args.get("category")
+#     results = get_articles(input, input_category)
+#     return(results)
+
+
+@app.route('/search', methods=['GET', 'POST'])
 def search():
     input = request.args.get("search")
     input_category = request.args.get("category")
     results = get_articles(input, input_category)
-    return(results)
+    return render_template('nyt.html', input = input, results = results)
+
+    # place = request.form['place']
+    # max_results = int(request.form['max_results'])
+    # radius = float(request.form['radius'])
+    # sort = True if request.form.get('sort') == 'true' else False
+    # results = wikipedia_locationsearch(place, max_results, radius, sort)
+    # return render_template('nyt.html')
+
+
+# @app.route("/search")
+# def search():
+#     input = request.args.get("search")
+#     input_category = request.args.get("category")
+#     results = get_articles(input, input_category)
+#     return(results)
--- newsfunctions.py
+++ newsfunctions.py
@@ -4,31 +4,38 @@ import urllib.error
 import json
 import keys
 import pprint
+import requests
+
+
 
 
 def extract_news_url(search,domain):
-    base_url = "https://newsapi.org/v2/everything"
-    url = (base_url + "?q=" + search + "&domain=" + domain + "&apiKey=" + keys.news_api_key)
+   url = "https://newsapi.org/v2/everything"
 
-    try:
-        x = urllib.request.urlopen(url)
-        articles = json.loads(x.read())['articles']
-        parsed_articles = []
-        for article in articles:
-            article_info = {
-                "title": article["title"],
-                "description": article["description"],
-                "source": article["source"]["name"],
-                "content": article["content"],
-                "author": article["author"],
-                "date": article["publishedAt"],
-                "link": article["url"]
-            }
-            parsed_articles.append(article_info)
-        return(parsed_articles)
 
-    except urllib.error.URLError as e:
-        print('The server couldn\'t fulfill the request.')
-        print(f'Error code: {e.code}')
+   querystring = {"q": search, "domains": domain,
+                  "apiKey": keys.news_api_key}
+   try:
+       response = requests.request("GET", url, params=querystring)
+       articles = response.json()['articles']
+       parsed_articles = []
+       for article in articles:
+           article_info = {
+               "title": article["title"],
+               "description": article["description"],
+               "source": article["source"]["name"],
+               "content": article["content"],
+               "author": article["author"],
+               "date": article["publishedAt"],
+               "link": article["url"]
+           }
+           parsed_articles.append(article_info)
+       return(parsed_articles)
 
-pprint.pprint(extract_news_url("finance"))
+
+   except urllib.error.URLError as e:
+       print('The server couldn\'t fulfill the request.')
+       print(f'Error code: {e.code}')
+
+
+pprint.pprint(extract_news_url("finance","techcrunch.com"))
